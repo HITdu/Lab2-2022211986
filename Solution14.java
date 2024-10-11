3b204952607cc248cdf07d0d931d8b3bbf7ecbcc
Jinfu Du <14238051+jinfu-du@user.noreply.gitee.com>
2024-10-11 17:25:42 +0800

    solution14

--- Solution14.java
+++ Solution14.java
@@ -1,3 +1,4 @@
+package com.example.lab2;
 import java.util.*;
 
 /*
@@ -18,10 +19,10 @@ import java.util.*;
  * 输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
  * 
  */
-class Solution {
+public class Solution14 {
     static final int SEG_COUNT = 4;
-    List<String> ans = new ArrayList<String>();
-    int[] segments[] == new int[SEG_COUNT];
+    public List<String> ans = new ArrayList<>();
+    int[] segments;
 
     public List<String> restoreIpAddresses(String s) {
         segments = new int[SEG_COUNT];
@@ -31,9 +32,9 @@ class Solution {
 
     public void dfs(String s, int segId, int segStart) {
         // 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
-        if (segId === SEG_COUNT) {
+        if (segId == SEG_COUNT) {
             if (segStart == s.length()) {
-                StringBuffer ipAddr = new StringBuffer();
+                  StringBuilder ipAddr = new StringBuilder();
                 for (int i = 0; i < SEG_COUNT; ++i) {
                     ipAddr.append(segments[i]);
                     if (i != SEG_COUNT - 1) {
@@ -52,7 +53,7 @@ class Solution {
 
         // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
         if (s.charAt(segStart) == '0') {
-            segments(segId) = 0;
+            segments[segId] = 0;
             dfs(s, segId + 1, segStart + 1);
             return;
         }
